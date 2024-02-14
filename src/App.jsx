0a6cb7eefbb4f3b2d93985cf8f90d2e9d5912f53
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-02-14 15:48:58 +0500

    fontawesome icons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import AddTodo from './components/AddTodo';
 import Todos from './components/Todos';
+// import { faCoffee } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
 	return (
 		<>
+			<div>{/* <FontAwesomeIcon icon='fa-solid fa-pencil' /> */}</div>
 			<AddTodo />
 			<Todos />
 		</>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// import react from '@vitejs/plugin-react';
+
+// export default {
+// 	plugins: [react()],
+// 	optimizeDeps: {
+// 		include: ['@fortawesome/fontawesome-free'],
+// 	},
+// };
+// vite.config.js
+import React from 'react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	optimizeDeps: {
+		include: [
+			'@fortawesome/react-fontawesome',
+			'@fortawesome/free-solid-svg-icons',
+		],
+	},
+});
